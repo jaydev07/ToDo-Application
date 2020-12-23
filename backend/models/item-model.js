78b7e73e89ec8bb59e4b264da358346c22405f93
Jaydev Bhavsar <jdbhavsar213@gmail.com>
2020-12-23 20:35:59 +0530

    User Auth is left

--- backend/models/item-model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const itemSchema = new mongoose.Schema({
-    name:{type:String , required:true},
-    listId:{type:mongoose.Types.ObjectId , required:true , ref:'Userlist'}
-});
-
-module.exports = mongoose.model('Item',itemSchema);
